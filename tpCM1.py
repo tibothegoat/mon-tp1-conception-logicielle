977e5c86b67db149ee3e5af98292ccc9b6fb1850
tibothegoat <tibo.negrier@gmail.com>
2024-01-15 15:54:41 +0100

    trois

--- tpCM1.py
+++ tpCM1.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
+import pytz # $ pip install pytz
+
+
+timezone_paris = pytz.timezone('Europe/Paris')
+
+current_time = datetime.now(timezone)
+
+current_time_formatted = current_time.strftime("%H:%M:%S")
+
+print(current_time_formatted))
+
+"""
+
 import datetime
 
 def main():
@@ -14,3 +29,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
