bb2b51f8d1b1973ce7236c527b1c2f94e08deb00
tibothegoat <tibo.negrier@gmail.com>
2024-01-22 14:16:25 +0100

    code du tp1

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Fichier généré automatiquement
-# Heure actuelle : 15:11:29
+from datetime import datetime
+
+import pytz # $ pip install pytz
+
+
+timezone_paris = pytz.timezone('Europe/Paris')
+
+current_time = datetime.now(timezone)
+
+current_time_formatted = current_time.strftime("%H:%M:%S")
+
+print(current_time_formatted)
